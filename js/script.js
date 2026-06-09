e2b8e240690a12052d281d0257745421d813483f
Thiago Lemos <lemost45@gmail.com>
2026-06-09 09:36:18 -0300

    republicando meu music player

--- js/script.js
+++ js/script.js
@@ -325,3 +325,12 @@ function fmt(s) {
 }
 
 render(tracks);
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('./sw.js')
+            .then((reg) => console.log('[SW] Registrado:', reg.scope))
+            .catch((err) => console.warn('[SW] Falhou:', err));
+    });
+}
